5d1c29a9d59fce6382efe3b7c8712555b753bce4
manisha <manisha.gupta@srijan.net>
2018-08-09 08:35:46 +0000

    sticky footer

--- themes/curemint/js/custom.js
+++ themes/curemint/js/custom.js
@@ -20,4 +20,14 @@ jQuery(document).ready(function() {
     slidesToShow: 16,
     slidesToScroll: 1
   });
+  //sticky footer
+  footerAlign();
 });
+
+function footerAlign() {
+  jQuery('.custom-footer').css('display', 'block');
+  jQuery('.custom-footer').css('height', 'auto');
+  var footerHeight = jQuery('.custom-footer').outerHeight();
+  jQuery('body').css('padding-bottom', footerHeight);
+  jQuery('.custom-footer').css('height', footerHeight);
+}
