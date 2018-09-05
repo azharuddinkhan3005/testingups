75b94b3b1da84745d243c4032ab2d1213a75278a
azharuddinkhan3005 <azharuddin.khan3005gmail.com>
2018-09-05 07:49:10 +0000

    uncommented install code

--- private/scripts/initialsetup.php
+++ private/scripts/initialsetup.php
@@ -1,6 +1,6 @@
 <?php
   // Installing Drupal.
-  //passthru('drush si standard --db-url=mysql://' . $_ENV['DB_USER'] . ':' . $_ENV['DB_PASSWORD'] . '@' . $_ENV['DB_HOST'] . ':' . $_ENV['DB_PORT'] . '/' . $_ENV['DB_NAME'] . ' --db-su=' . $_ENV['DB_USER'] . ' --db-su-pw=' . $_ENV['DB_PASSWORD'] . ' --site-name="Curemint" --account-name="curemint" --account-pass="curemint" --site-mail="azharuddin.khan3005@gmail.com" --account-mail="azharuddin.khan3005@gmail.com" -y');
+  passthru('drush si standard --db-url=mysql://' . $_ENV['DB_USER'] . ':' . $_ENV['DB_PASSWORD'] . '@' . $_ENV['DB_HOST'] . ':' . $_ENV['DB_PORT'] . '/' . $_ENV['DB_NAME'] . ' --db-su=' . $_ENV['DB_USER'] . ' --db-su-pw=' . $_ENV['DB_PASSWORD'] . ' --site-name="Curemint" --account-name="curemint" --account-pass="curemint" --site-mail="azharuddin.khan3005@gmail.com" --account-mail="azharuddin.khan3005@gmail.com" -y');
   //Changing the system uuid.
   passthru('drush config-set "system.site" uuid "f6a064fd-7bb6-4c91-84a8-e1a2a9478585" -y');
   // Deleting the default shortcuts.
